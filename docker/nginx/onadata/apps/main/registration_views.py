abf031aec3aabd996459a9a02d4db70d42cd3769
Robert Reihs <robert.reihs@medunigraz.at>
2018-06-14 14:57:27 +0200

    Update nginx add folder for build

--- docker/nginx/onadata/apps/main/registration_views.py
+++
@@ -1,9 +0,0 @@
-from registration.backends.default.views import RegistrationView
-
-
-class FHRegistrationView(RegistrationView):
-    def register(self, form):
-        new_user = super(FHRegistrationView, self).register(form)
-        form.save_user_profile(new_user)
-
-        return new_user
--- docker/nginx/onadata/libs/baseviewset.py
+++
@@ -1,2 +0,0 @@
-class DefaultBaseViewset(object):
-    pass
